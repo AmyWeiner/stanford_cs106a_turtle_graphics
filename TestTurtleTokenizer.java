cc2ef3f11df905d42713bac1d03fa681da667da9
S <s>
2012-05-14 15:43:54 -0700

    {"type":"TI", "runs":"0"}

--- TestTurtleTokenizer.java
+++ TestTurtleTokenizer.java
@@ -3,12 +3,13 @@ import acm.program.ConsoleProgram;
 
 public class TestTurtleTokenizer extends ConsoleProgram{
 	public void run() {
-		String program = readLine("Enter a turtle program: ");
-		TurtleTokenizer tokenizer = new TurtleTokenizer(program);
-		while (tokenizer.hasMoreTokens()) {
-			String token = tokenizer.nextToken();
-			println(token);
+		while (true) {
+			String program = readLine("Enter a turtle program: ");
+			TurtleTokenizer tokenizer = new TurtleTokenizer(program);
+			while (tokenizer.hasMoreTokens()) {
+				String token = tokenizer.nextToken();
+				println(token);
+			}
 		}
 	}
-
 }
