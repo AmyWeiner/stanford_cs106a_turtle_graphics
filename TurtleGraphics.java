95e2e4d490e9fe6ff6b989e761470d535f6b9fea
S <s>
2012-05-14 08:38:36 -0700

    {"type":"TI", "runs":"0"}

--- TurtleGraphics.java
+++ TurtleGraphics.java
@@ -67,7 +67,7 @@ public class TurtleGraphics extends GraphicsProgram {
 	}
 
 	private void translateToCommand(String token) {
-		token = token.toUpperCase();
+		//token = token.toUpperCase();
 		char ch = token.charAt(0);
 		switch (ch){
 		case 'F': 
